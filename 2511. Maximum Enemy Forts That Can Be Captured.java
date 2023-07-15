f72089291c02e760b457038ef1b7a668672e8fb7
Abhiram <107067728+abhiramrote@users.noreply.github.com>
2023-07-15 23:02:25 +0530

    Easy 2511. Maximum Enemy Forts That Can Be Captured.java

---
+++ 2511. Maximum Enemy Forts That Can Be Captured.java
@@ -0,0 +1,27 @@
+int sum = 0;// for  calculating cumulative sum
+        int ret = 0; // for returning the max string of zeros between -1 and 1
+        int count = 0; // for counting and comparing string of zeros
+        
+
+        for(int i = 0 ; i < forts.length ; i++){
+            
+            if(forts[i] != 0) // counting starts and ends at non-zero fort
+            {
+  
+                sum += forts[i]; // s counts the cumulative sum from the last non-zero fort
+                if(sum == 0) 
+                    ret = Math.max(ret, count);// if s = 0, it means we are either at start or end, hence compare
+                
+                sum = forts[i];//cumulative sum resets to current value
+                count = 0;//count resets at non-zero fort value
+                
+            }
+            
+            else
+            {
+                if(sum != 0)
+                count++; //count if we already encountered a non-zero fort         
+                
+            }
+        }   
+        return ret;
